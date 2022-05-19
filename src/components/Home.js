c83a63937286c9eb4705ad1855b38905892af55b
Mariana Hodniki Davie <marianahodniki1991@gmail.com>
2022-05-19 16:06:13 -0600

    adding home container and css on js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import styled from 'styled-components'
 
 function Home() {
     return (
-        <div>
-            home
-        </div>
+        
+            <container>
+              Home
+            </container>
+           
+        
     );
 }
 
 export default Home;
+
+const Container = styled.div`
+height: 100vh;
+`
