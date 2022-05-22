6e459f37c46701f21c246c2c3618b3bb9e7b5918
Mariana Hodniki Davie <marianahodniki1991@gmail.com>
2022-05-22 15:50:57 -0600

    finishe props section

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,70 +1,67 @@
 import React from 'react';
 import styled  from "styled-components"
 
-function Section( {title, description, leftBtnText, rightBtntext, }) {
+function Section({ title, description, backgroundImg, leftBtn, rightBtn }) {
     return (
-        <Wrap>
-            <ItemText>
-                <h1>{title}</h1>
-                <p>{description}</p>
-            </ItemText>
+      <Wrap bgImg={backgroundImg}>
+        
+          <ItemText>
+            <h1>{title}</h1>
+            <p>{description}</p>
+          </ItemText>
+        
+  
+        <Buttons>
+          
+            <ButtonGroup>
+              <LeftButton>
+                  {leftBtn}
+              </LeftButton>
 
-            <Buttons>
-                <ButtonGroup>
-                    
-                    <LeftButton>
-                       {leftBtnText}
-                    </LeftButton>
-
-                    <RightButton>
-                      {rightBtntext}
-                    </RightButton>                   
-
-                </ButtonGroup>
-
-                <DownArrow src = "/images/down-arrow.svg" />
-            </Buttons>
-        </Wrap>
+              <RightButton>
+                  {rightBtn}
+            </RightButton> 
             
-       
+            </ButtonGroup>
+          
+  
+          <DownArrow src="/images/down-arrow.svg" />
+        </Buttons>
+      </Wrap>
     );
-}
-
-export default Section;
-
-const Wrap = styled.div`
+  }
+  
+  export default Section;
+  
+  const Wrap = styled.div`
     width: 100vw;
     height: 100vh;
     background-size: cover;
     object-fit: cover;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-image: url('/images/model-s.jpg');
+    background-position: 50% 50%;
+    backgroun-repeat: no-repeat;
+    background-image: url("/images/model-s.jpg");
     display: flex;
     flex-direction: column;
     justify-content: space-between;
     align-items: center;
-
-`;
-
-const ItemText = styled.div`
+    background-image: ${(props) => `url("/images/${props.bgImg}")`};
+  `;
+  
+  const ItemText = styled.div`
     padding-top: 15vh;
     text-align: center;
-
-`;
-
-const ButtonGroup = styled.div`
+  `;
+  
+  const ButtonGroup = styled.div`
     display: flex;
-    margin-bottom: 30px;
     @media (max-width: 768px) {
-        flex-direction: column;
-      }
-
-
-`;
-
-const LeftButton = styled.div`
-    background-color: rgba(23, 26, 32, 0.8);
+      flex-direction: column;
+    }
+  `;
+  
+  const LeftButton = styled.div`
+    background-color: rgba(0, 0, 0, 0.9);
     height: 40px;
     width: 256px;
     color: white;
@@ -80,25 +77,19 @@ const LeftButton = styled.div`
     margin: 4px 8px;
     margin-bottom: 32px;
     padding: 5px 40px;
-
-`;
-
-const RightButton = styled(LeftButton)` 
-    background: white;
-    opacity: 0.65;
+  `;
+  
+  const RightButton = styled(LeftButton)`
+    background-color: white;
     color: black;
-
-`;
-
-const DownArrow = styled.img `
-    margin-top: 20px;
+    opacity: 0.65;
+  `;
+  
+  const DownArrow = styled.img`
     height: 40px;
-    overflow-x: hidden;
+    margin-top: 20px;
+    oveflow-x: hidden;
     animation: animateDown infinite 1.5s;
-
-`;
-
-const Buttons = styled.div`
-
-`;
-
+  `;
+  
+  const Buttons = styled.div``;
