3c8cca90c0266f194f59d74879e249831a410ed7
Mariana Hodniki Davie <marianahodniki1991@gmail.com>
2022-05-26 12:19:26 -0600

    material ui not working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
@@ -6,7 +6,7 @@ import Home from './components/Home';
 function App() {
   return (
     <div className="App">
-     {/* <Header /> */}
+     <Header />
      <Home />
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,36 @@
 import React from 'react';
-import styled  from "styled-components"
+import styled  from "styled-components";
+import materialui from '@mui/material'
+
+import MenuIcon from "@mui/icons-material/Menu";
+
 
 function Header() {
         return (
           
         <Container>
             <a>
-                <img src="/images/logo.png" alt="tesla logo"/>
+                <img src="/images/logo.svg" alt="tesla logo"/>
             </a>
+            <Menu>
+            
+                <a href="#"> Model S </a>
+                <a href="#"> Model X </a>
+                <a href="#"> Model Y </a>
+                <a href="#"> Model 3 </a>            
+           
+            </Menu>
+
+            <RightMenu>
+
+                <a href="#"> Shop</a>
+                <a href="#"> Tesla Account</a>
+                
+                <MyMenu/>
+
+                
+
+            </RightMenu>
 
         </Container>
         )
@@ -17,5 +40,47 @@ function Header() {
 export default Header;
 
 const Container = styled.div `
+    min-height:60px;
+    position: fixed;
+    display: flex;
+    align-items: center;
+    padding: 0 20px;
+    top: 0;
+    left: 0;
+    right: 0;
+
+
+`
+const Menu = styled.div `
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex: 1;
+    
+    a {
+        font-weight: 600;
+        text-transform: uppercase;
+        padding: 0 10px;
+        flex-wrap: nowrap;
+    }
+`
+
+const RightMenu = styled.div `
+
+display: flex;
+align-items: center;
+
+
+a {
+    font-weight: 600;
+    text-transform: uppercase;
+    margin-right: 10px;
+    flex-wrap: nowrap;
+}
+
+`
+const MyMenu = styled( MenuIcon ) `
+
+    cursor: pointer;
 
 `
